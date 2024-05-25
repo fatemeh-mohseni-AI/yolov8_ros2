275f7df8cab597192f7c11deb236d9ed54c5e01f
fatemeh_mohseni <mic.ninfateni@gmail.com>
2024-05-25 16:46:15 +0330

    run_prod.sh added

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,8 @@ source /opt/ros/foxy/setup.bash
 source ~/ros2_ws/install/setup.bash
 
 # Run the ROS 2 launch command in the background
-ros2 launch yolov8_bringup yolov8.launch.py model:=/root/ros2_ws/src/Ros2_Yolov8/model/best.pt device:=CPU
+#ros2 launch yolov8_bringup yolov8.launch.py model:=/root/ros2_ws/src/Ros2_Yolov8/model/best.pt device:=CPU
 
 # Run the Python video publisher script
 #python3 ~/ros2_ws/src/Ros2_Yolov8/test/player.py
+sleep 10000000000
